6ea748e9c21ed7331d90e4fedce7e340e48f98a4
davidbs09 <davidbissacodasilva@gmail.com>
2025-03-03 15:02:59 -0300

    search method implemented

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -14,8 +14,42 @@ document.querySelector('#search').addEventListener('submit', async (event) => {
     const results = await fetch(apiUrl);
     const json = await results.json();
 
-    console.log(json);
+    if (json.cod === 200) {
+        showInfo({
+            city: json.name,
+            country: json.sys.country,
+            temp: json.main.temp,
+            tempMax: json.main.temp_max,
+            tempMin: json.main.temp_min,
+            description: json.weather[0].description,
+            tempIcon: json.weather[0].icon,
+            windSpeed: json.wind.speed,
+            humidity: json.main.humidity,
+        });
+    } else {
+        showAlert(`Cidade não encontrada...
+
+            <img src="src/img/notFound.svg" alt="Cidade não encontrada">
+            `);
+    };
 });
+
+function showInfo(json){
+    showAlert('');
+
+    document.querySelector("#weather").classList.add('show');
+
+    document.querySelector('#title').innerHTML = `${json.city}, ${json.country}`;
+
+    document.querySelector('#temp_value').innerHTML = `${json.temp.toFixed(1).toString().replace('.', ',')} <sup>C°</sup>`;
+    document.querySelector('#temp_description').innerHTML = `${json.description}`;
+    document.querySelector('#temp_img').setAttribute('src', `https://openweathermap.org/img/wn/${json.tempIcon}@2x.png`)
+
+    document.querySelector('#temp_max').innerHTML = `${json.tempMax.toFixed(1).toString().replace('.', ',')} <sup>C°</sup>`;
+    document.querySelector('#temp_min').innerHTML = `${json.tempMin.toFixed(1).toString().replace('.', ',')} <sup>C°</sup>`;
+    document.querySelector('#humidity').innerHTML = `${json.humidity}%`;
+    document.querySelector('#wind').innerHTML = `${json.windSpeed.toFixed(1)}km/h`;
+}
 
 function showAlert(msg) {
     document.querySelector('#alert').innerHTML = msg;
